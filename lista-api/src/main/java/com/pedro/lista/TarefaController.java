a0846102bf39e0d17bf25c70a28e6480d6baf906
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2025-06-11 02:06:18 +0000

    Wip

--- lista-api/src/main/java/com/pedro/lista/TarefaController.java
+++ lista-api/src/main/java/com/pedro/lista/TarefaController.java
@@ -2,15 +2,12 @@ package com.pedro.lista;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 @RequestMapping("/tarefas")
 public class TarefaController {
 
     @GetMapping
-    public List<String> listarTarefas() {
-        return Arrays.asList("Tarefa 1", "Tarefa 2", "Tarefa 3");
+    public String listarTarefas() {
+        return "23h e eu aqui vegetando";
     }
 }
