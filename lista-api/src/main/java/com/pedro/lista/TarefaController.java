6f79be0ad22a3da76bb1732e5902ff52114788cd
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2025-06-13 00:44:54 +0000

    Wip

--- lista-api/src/main/java/com/pedro/lista/TarefaController.java
+++ lista-api/src/main/java/com/pedro/lista/TarefaController.java
@@ -1,8 +1,15 @@
 package com.pedro.lista;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/tarefas")
@@ -12,4 +19,19 @@ public class TarefaController {
     public String listarTarefas() {
         return "forward:/tarefas.html"; 
     }
+
+
+    @GetMapping("/multiplicar-tres")
+    @ResponseBody
+    public ResponseEntity<?> multiplicarTres(@RequestParam double a, @RequestParam double b, @RequestParam double c) {
+        Map<String, Object> response = new HashMap<>();
+        double result = a * b * c;
+        response.put("operação", "multiplicar-tres");
+        response.put("a", a);
+        response.put("b", b);
+        response.put("c", c);
+        response.put("resultado", result);
+        return ResponseEntity.ok(response);
+    }
 }
+
