1004e702e5d2f8572a020bb63adc564e7e77865f
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2025-06-15 22:34:38 +0000

    Teste com fetch

--- lista-api/src/main/java/com/pedro/lista/TarefaController.java
+++ lista-api/src/main/java/com/pedro/lista/TarefaController.java
@@ -23,7 +23,7 @@ public class TarefaController {
 
     @GetMapping("/multiplicar-tres")
     @ResponseBody
-    
+
     public ResponseEntity<?> multiplicarTres(@RequestParam double a, @RequestParam double b, @RequestParam double c) {
         Map<String, Object> response = new HashMap<>();
         double result = a * b * c;
@@ -49,6 +49,33 @@ public class TarefaController {
         response.put("a", a);
         response.put("b", b);
         response.put("resultado", result);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/media-ponderada")
+    @ResponseBody
+
+    public ResponseEntity<?> calcularMediaPonderada(@RequestParam double a, @RequestParam double b) {
+        Map<String, Object> response = new HashMap<>();
+        double mediaPonderada = (a * 2 + b * 3) / 5;
+        response.put("operação", "media-ponderada");
+        response.put("a", a);
+        response.put("b", b);
+        response.put("resultado", mediaPonderada);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/desconto-dez")
+    @ResponseBody
+
+    public ResponseEntity<?> descontoDez(@RequestParam double a){
+        Map<String, Object> response = new HashMap<>();
+        double desconto = a * 0.10;
+        double valorComDesconto = a - desconto;
+
+        response.put("operação", "desconto-dez");
+        response.put("a", a);
+        response.put("valor com desconto", valorComDesconto);
         return ResponseEntity.ok(response);
     }
 }
