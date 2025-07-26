abd3bf4ac0e67a68e17b3839ad6cc7c9e9487e0c
BeemerCodes <detonainyoutub@gmail.com>
2025-07-25 21:37:45 -0300

    Configutando para rodar localmente

--- lista-api/src/main/java/PasswdRequest.java
+++
@@ -1,6 +0,0 @@
-package com.pedro.lista;
-public class PasswdRequest {
-    private String a;
-    public String getA() { return a; }
-    public void setA(String a) { this.a = a; }
-}
---
+++ lista-api/src/main/java/com/pedro/lista/RequestUmValor.java
@@ -0,0 +1,10 @@
+package com.pedro.lista;
+public class RequestUmValor {
+    private double a;
+    public double getA() {
+        return a;
+    }
+    public void setA(double a) {
+        this.a = a;
+    }
+}
